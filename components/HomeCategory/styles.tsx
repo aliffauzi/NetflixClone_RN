6f797c89ed0f410c76301c0673345926d8880bc2
T3sultan <incognito.soul011@gmail.com>
2021-07-04 00:38:57 +0600

    Home Screen ui

---
+++ components/HomeCategory/styles.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles=StyleSheet.create({
+    
+    imageStyle:{
+        width: 110,
+        height: 170,
+        resizeMode:'contain',
+        borderRadius:8,
+        margin: 5
+    },
+    headText:{
+        fontSize:20,
+        fontWeight:"bold"
+    }
+
+});
+export default styles;
